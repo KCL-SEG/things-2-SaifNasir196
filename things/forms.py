f5b4686c4c10a9c8ba7186d5c675cc9c07286755
Saif <muhammadsaif713@gmail.com>
2023-12-07 02:04:09 +0000

    complete exercose

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,20 @@
+from django import forms
+from .models import Thing
 """Forms of the project."""
 
 # Create your forms here.
+
+
+# ThingForm, that contains the fields name, description, and quantity, but not created_at.
+
+# def ThingForm( forms.ModelForm):
+class ThingForm(forms.ModelForm):
+    """Thing form."""
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']        
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(attrs={'min': 0}),
+        }
+
